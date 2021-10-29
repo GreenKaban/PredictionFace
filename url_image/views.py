e661534d39b870b39f70f5a77a74fc6472c55f99
greenkaban <kochetkov@ispras.ru>
2021-10-29 19:17:23 +0300

    add error handling

--- url_image/views.py
+++ url_image/views.py
@@ -16,8 +16,7 @@ def index(request):
     default_portret_url = "https://example1.jpg"
     default_style_url = "https://example2.jpg"
     
-    portret_err = False
-    style_err = False
+    photo_err = False
 	# if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -29,19 +28,19 @@ def index(request):
             try:
                 portret, portret64 = api.get_img_and_base64(form.cleaned_data['portret_url'])
             except Exception as e:
-                portret_err = True
+                photo_err = True
                 # print("portret err ", e, portret_err)
 
             try:
                 style, style64 = api.get_img_and_base64(form.cleaned_data['style_url'])
             except Exception as e:
-                style_err = True
+                photo_err = True
                 # print(" style err ", e, style_err)
 
                 # print("\nerror in get_img_and_base64\n")
                 # form.cleaned_data['style_url'] = "Sorry! Image can't be loaded from that link);"
             
-            if not portret_err and not style_err:
+            if not photo_err:
                 # print("\nmask\n")
                 # mask, mask64 = api.get_person_mask_and_mask64(portret)
                 result64 = api.merge_style_and_person( portret, style)
@@ -52,11 +51,11 @@ def index(request):
                 "not_valid_form": False}
                 return render(request, 'url_image/index.html', context)
             else:
-                context = {'form': form, "portret_err": portret_err, "style_err": style_err,\
+                context = {'form': form, "photo_err": photo_err, \
                 "load_static_examples": False, "new_imgs": False, "not_valid_form": False}
                 return render(request, 'url_image/index.html', context)
         else:
-            context = {'form': form, "portret_err": False, "style_err": False, \
+            context = {'form': form, "photo_err": False, \
             "load_static_examples": False, "new_imgs": False, "not_valid_form": True}
             return render(request, 'url_image/index.html', context)
 
@@ -65,7 +64,7 @@ def index(request):
         form = UrlsForm(initial={'portret_url': default_portret_url,\
                         "style_url": default_style_url})
         context = {'form': form, "load_static_examples": True, "new_imgs": False,\
-        "portret_err": False, "style_err": False, "not_valid_form": False}
+        "photo_err": False, "not_valid_form": False}
 
         return render(request, 'url_image/index.html', context)
 
