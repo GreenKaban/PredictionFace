512bc60fb93530d9679146a8317f4d4abc4bfc09
Sveta <shchaulova.sv@phystech.edu>
2021-12-15 12:26:27 +0300

    some refactoring

--- url_image/backend/api.py
+++ url_image/backend/api.py
@@ -2,13 +2,12 @@ import base64
 import io
 import numpy as np
 import requests
-from imageio import imsave, imread
+from imageio import imsave
 from PIL import Image
-import PIL
 from deepface import DeepFace
 
 
-def get_img_and_base64(url):
+def get_img_and_base64(url: str) -> (Image.Image, str):
     """
     returns image in base64 format (a string)
     that can be passed to html as context and rendered without saving to drive
@@ -20,12 +19,12 @@ def get_img_and_base64(url):
     imsave(fmem, img_np, 'png')
     fmem.seek(0)
     img64 = base64.b64encode(fmem.read()).decode('utf-8')
-    return (img, img64)
+    return img, img64
 
 
-def match_photo(mask, person):
+def match_photo(photo_1: Image.Image, photo_2: Image.Image) -> str:
     try:
-        if DeepFace.verify(np.array(mask), np.array(person), model_name='ArcFace')['verified']:
+        if DeepFace.verify(np.array(photo_1), np.array(photo_2), model_name='ArcFace')['verified']:
             back_np = 'Same'
 
         else:
@@ -33,9 +32,4 @@ def match_photo(mask, person):
     except ValueError:
         back_np = 'Face not found'
 
-    # fmem = io.BytesIO()
-    # imsave(fmem, back_np, 'png')
-    # fmem.seek(0)
-    # merged64 = base64.b64encode(fmem.read()).decode('utf-8')
     return back_np
-    # patch_under_person[:,:,0][mask_np > 0] = 0
